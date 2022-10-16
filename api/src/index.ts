6b6303967b1818b080c3b7af9dd4da3e6c45d387
bluemountains88 <m.dariouk@gmail.com>
2022-10-15 23:09:38 -0300

    Add connection to MongoDB
    Refactor on app listening

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,24 @@
 import app from "./app";
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Listening in port ${PORT}`);
-});
+
+const start = async () => {
+  if(!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+  
+  app.listen(PORT, () => {
+    console.log(`Listening in port ${PORT}`);
+  });
+}
+
+start();
+
